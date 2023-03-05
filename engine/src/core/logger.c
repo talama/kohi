d8639fc52246daf1bba4a62aa88b3a4c2192d97e
Talama <talama@proton.me>
2023-03-05 15:30:20 +0100

    feat: windows platform initial setup

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,5 +1,6 @@
 #include "logger.h"
 #include "assert.h"
+#include "platform/platform.h"
 
 // TODO: temporary
 #include <stdarg.h>
@@ -7,41 +8,46 @@
 #include <string.h>
 
 b8 initialize_logging() {
-  // TODO create log file.
-  return TRUE;
+    // TODO create log file.
+    return TRUE;
 }
 
 void shutdown_logging() {
-  // TODO: cleanup logging/write queued entries/
+    // TODO: cleanup logging/write queued entries/
 }
 
-void log_output(log_level level, const char *message, ...) {
-  const char *level_strings[6] = {"[FATAl]: ", "[ERROR]: ", "[WARN]: ",
-                                  "[INFO]: ",  "[DEBUG]: ", "[TRACE]: "};
-  // b8 is_error = level < 2;
+void log_output(log_level level, const char* message, ...) {
+    const char* level_strings[6] = {"[FATAl]: ", "[ERROR]: ", "[WARN]: ",
+                                    "[INFO]: ", "[DEBUG]: ", "[TRACE]: "};
+    b8 is_error = level < LOG_LEVEL_WARN;
 
-  // Impose a 32k character limit on a single log entry.
-  char out_message[32000];
-  memset(out_message, 0, sizeof(out_message));
+    // Impose a 32k character limit on a single log entry.
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
+    memset(out_message, 0, sizeof(out_message));
 
-  // Handles variadic arguments
-  __builtin_va_list arg_ptr;
-  va_start(arg_ptr, message);
-  // print the formatted message.
-  vsnprintf(out_message, 32000, message, arg_ptr);
-  va_end(arg_ptr);
+    // Handles variadic arguments
+    __builtin_va_list arg_ptr;
+    va_start(arg_ptr, message);
+    // print the formatted message.
+    vsnprintf(out_message, msg_length, message, arg_ptr);
+    va_end(arg_ptr);
 
-  // Prepend the appropriate level string to the output message.
-  char prepend_message[32000];
-  sprintf(prepend_message, "%s%s\n", level_strings[level], out_message);
+    // Prepend the appropriate level string to the output message.
+    char prepend_message[msg_length];
+    sprintf(prepend_message, "%s%s\n", level_strings[level], out_message);
 
-  // TODO: platform specific output.
-  printf("%s", prepend_message);
+    // Platform-specific output.
+    if (is_error) {
+        platform_console_write_error(prepend_message, level);
+    } else {
+        platform_console_write(prepend_message, level);
+    }
 }
 
-KAPI void report_assertion_failure(const char *expression, const char *message,
-                                   const char *file, i32 line) {
-  log_output(LOG_LEVEL_FATAL,
-             "Assertion failure: %s, message: '%s', in file %s, line %d\n",
-             expression, message, file, line);
+void report_assertion_failure(const char* expression, const char* message,
+                              const char* file, i32 line) {
+    log_output(LOG_LEVEL_FATAL,
+               "Assertion failure: %s, message: '%s', in file %s, line %d\n",
+               expression, message, file, line);
 }
--- testbed/src/main.c
+++ testbed/src/main.c
@@ -2,15 +2,22 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
-int main(void){
-  KFATAL("A fatal test message: %f", 3.14f);
-  KERROR("A error test message: %f", 3.14f);
-  KWARN("A warn test message: %f", 3.14f);
-  KINFO("A info test message: %f", 3.14f);
-  KDEBUG("A debug test message: %f", 3.14f);
-  KTRACE("A trace test message: %f", 3.14f);
+// TODO: Test
+#include <platform/platform.h>
 
-  KASSERT(1 == 0);
-  KASSERT_MSG(1 == 0, "assertion message test");
-  return 0;
+int main(void) {
+    KFATAL("A fatal test message: %f", 3.14f);
+    KERROR("A error test message: %f", 3.14f);
+    KWARN("A warn test message: %f", 3.14f);
+    KINFO("A info test message: %f", 3.14f);
+    KDEBUG("A debug test message: %f", 3.14f);
+    KTRACE("A trace test message: %f", 3.14f);
+
+    platform_state state;
+    if (platform_startup(&state, "Kohi Engine Testbed", 100, 100, 1280, 720)) {
+        while (TRUE) {
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
 }
